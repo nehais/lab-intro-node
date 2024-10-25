a0b3e2fe68a754afc99678db343ebcb7c603b88c
nehais <nehasinha82@gmail.com>
2024-10-25 10:59:44 +0200

    Lab done

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items  = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    let eleIdx = this.items.findIndex(e => e > item);
+    eleIdx     = (eleIdx === -1 ? this.length : eleIdx);
+    this.items.splice(eleIdx, 0, item);
 
-  get(pos) {}
+    this.length = this.items.length;
+  }
 
-  max() {}
+  get(pos) {
+    if (pos >= (this.length)){
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  min() {}
+  max() {
+    if (this.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    return this.items[this.length-1];
+  }
 
-  sum() {}
+  min() {
+    if (this.length === 0){
+      throw new Error("Empty SortedList");
+    }
+    return this.items[0];
+  }
 
-  avg() {}
+  sum() {
+    return this.items.reduce((acc, curr) => acc + curr, 0);
+  }
+
+  avg() {
+    if (this.length === 0){
+      throw new Error("Empty SortedList");
+    }
+    return this.sum()/this.length;
+  }
 }
 
 module.exports = SortedList;
